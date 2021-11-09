6f7aed3640dd7a1e2c3061425ccc375a4e1dcb05
Colby Sawyer <sawyerc17@students.ecu.edu>
2021-11-09 16:50:42 -0500

    Lora from LoraWAN

--- node.py
+++ node.py
@@ -10,10 +10,27 @@ import board
 import digitalio
 import adafruit_ssd1306
 from adafruit_tinylora.adafruit_tinylora import TTN, TinyLoRa
+# Import the RFM9x radio module.
+import adafruit_rfm9x
 
 FEATHER_ID = 1
 
 i2c = busio.I2C(board.SCL, board.SDA)
+
+# Button A
+btnA = DigitalInOut(board.D5)
+btnA.direction = Direction.INPUT
+btnA.pull = Pull.UP
+
+# Button B
+btnB = DigitalInOut(board.D6)
+btnB.direction = Direction.INPUT
+btnB.pull = Pull.UP
+
+# Button C
+btnC = DigitalInOut(board.D12)
+btnC.direction = Direction.INPUT
+btnC.pull = Pull.UP
 
 # 128x32 OLED Display
 reset_pin = DigitalInOut(board.D4)
@@ -29,13 +46,17 @@ cs = DigitalInOut(board.CE1)
 irq = DigitalInOut(board.D22)
 rst = DigitalInOut(board.D25)
 
-#Node specific info (ABP Activation)
-devaddr = bytearray([0x26, 0x0C, 0x48, 0x7E])
-nwkey = bytearray([0x1C, 0xD3, 0x25, 0xAA, 0x64, 0x1D, 0x0A, 0x7E, 0x38, 0x01, 0x3E, 0xE4, 0xDE, 0xA9, 0xCD, 0xE0])
-app = bytearray([0x40, 0xEF, 0xAD, 0xBE, 0x0D, 0x62, 0xDF, 0x22, 0xDE, 0xFF, 0xAE, 0x71, 0x1E, 0x4D, 0xEA, 0xF8])
+# Lora
+rfm9x = adafruit_rfm9x.RFM9x(spi,cs,rst, 915.0)
+rfm9x.tx_power = 23
+prev_packet = None
 
-ttn_config = TTN(devaddr, nwkey, app, country='US')
-lora = TinyLoRa(spi, cs, irq, rst, ttn_config, channel=6)
+# LoRAWAN
+#devaddr = bytearray([0x26, 0x0C, 0x48, 0x7E])
+#nwkey = bytearray([0x1C, 0xD3, 0x25, 0xAA, 0x64, 0x1D, 0x0A, 0x7E, 0x38, 0x01, 0x3E, 0xE4, 0xDE, 0xA9, 0xCD, 0xE0])
+#app = bytearray([0x40, 0xEF, 0xAD, 0xBE, 0x0D, 0x62, 0xDF, 0x22, 0xDE, 0xFF, 0xAE, 0x71, 0x1E, 0x4D, 0xEA, 0xF8])
+#ttn_config = TTN(devaddr, nwkey, app, country='US')
+#lora = TinyLoRa(spi, cs, irq, rst, ttn_config, channel=6)
 
 sensor_data = bytearray(7)
 
@@ -56,11 +77,12 @@ while True:
     sensor_data[4] = humid_val & 0xff
 
     print('Sending packet .....')
-    lora.send_data(sensor_data, len(sensor_data), lora.frame_counter)
+    rfm9x.send(bytes(str(temp_val))
+    #lora.send_data(sensor_data, len(sensor_data), lora.frame_counter)
     print('Temperature: ' + str(temp_val) + '\t' + 'Humidity: ' + str(humid_val))
     print('Sent: \t' + datetime.now().strftime("%H:%M:%S.%f"))
     print("Packet Sent!\n\n")
-    lora.frame_counter += 1
+    #lora.frame_counter += 1
     display.text('Sent Data to TTN!' , 15, 15, 1)
     print('Data sent!')
     display.show()
