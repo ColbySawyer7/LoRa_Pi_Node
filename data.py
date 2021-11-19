fd96a53ada901a46a59c9b6e2a64fa704f84cbb4
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-11-19 09:02:41 -0500

    Added BME280 support

--- data.py
+++ data.py
@@ -1,10 +1,21 @@
 import random
+import time
+from adafruit_bme280 import basic as adafruit_bme280
+import board
 
 def get_data():
+    # Create sensor object, using the board's default I2C bus.
+    i2c = board.I2C()   # uses board.SCL and board.SDA
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+    # change this to match the location's pressure (hPa) at sea level
+    bme280.sea_level_pressure = 1013.25
+
     sensor_data = bytearray(7)   
 
-    temp_val = 75 + random.randint(0,25)
-    humid_val = 1000 - random.randint(0,250)
+    temp_val = bme280.temperature
+    humid_val = bme280.relative_humidity
+    press_val = bme280.pressure
 
     sensor_data[0] = 1
     # Temperature data
@@ -13,5 +24,8 @@ def get_data():
     # Humidity data
     sensor_data[3] = (humid_val >> 8) & 0xff
     sensor_data[4] = humid_val & 0xff
-
+    # Pressure data
+    sensor_data[5] = (press_val >> 8) & 0xff
+    sensor_data[6] = press_val & 0xff
+    
     return sensor_data
